d5ef745c502708dbdf5f73739d5890c6d85d0cc1
Jahangeer <writetojahangeer@gmail.com>
2025-10-02 22:41:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AppRouter } from './router/AppRouter'
 import { queryClient } from './lib/queryClient'
 import './globals.css'
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <AppRouter />
       </HelmetProvider>
       {/* TanStack Query DevTools - only in development */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </React.StrictMode>
 )
